c24ba1f5ea32081fc8f774c3a124b81f74dd1d61
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 20:05:30 +0630

    update

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('Hello JS');
+// mutable and immutable
+
+let originalUser = {
+  username: 'dceddia',
+  firstName: 'Dave',
+  lastName: 'Ceddia',
+  address: 'Mandalay',
+};
+
+const { address, ...userWithoutAddress } = originalUser;
+console.log(originalUser);
