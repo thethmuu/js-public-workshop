8abf0c6f20ebf7e331268b877a35d4299143c80e
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 20:37:06 +0630

    update

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 'use strict';
 
 console.log('Hello JS');
+// slice & splice
+const nums = [1, 2, 3, 4, 5, 6, 7];
+// splice (startingIndex, numberOfItem)
+// slice (startingIndex, endingIndex(not included itself))
