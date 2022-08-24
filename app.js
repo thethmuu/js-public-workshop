0442e1121e43d2d974bdcab0ac6e7f03a9a1de59
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 19:50:17 +0630

    update

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 'use strict';
 
 console.log('Hello JS');
+const nums = [1, 2, 3, 4];
+// const user = {
+//   name: 'Thet',
+//   age: 26,
+//   grade: 'A',
+//   level: 5,
+// };
+
+// const { name, ...otherProperties } = user;
+// console.log(otherProperties);
+
+const [a, b, ...restNums] = nums;
+console.log(restNums);
+
+const students = ['first', 'second', 'three', 'four', 'five'];
+
+const [first, second, ...noDiscount] = students;
+
+console.log(noDiscount);
