9e4842d9ecbb70f7860d4b202e87eb2d5b78dca3
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 20:41:25 +0630

    update

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('Hello JS');
+// map -> return new array modifying data on original array
+const users = [
+  {
+    id: 1,
+    name: 'Thet',
+  },
+  {
+    id: 2,
+    name: 'Thet',
+  },
+  {
+    id: 3,
+    name: 'Thet',
+  },
+];
+const allNames = users.map((user) => {
+  return user.name;
+});
+
+console.log(allNames);
