585449dba226c96d4c7dc8bac96c9d16b16ef375
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 18:33:59 +0630

    arrow functions

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-'use strict';
+// 'use strict';
+const getName = (name) => {
+  return name;
+};
 
-console.log('Hello JS');
+// implicit return
+const getNameNew = (name) => name;
+
+// React code
+// function CardComponent() {
+//     return (
+//       <div class='card'>
+//         <h1>Title</h1>
+//         <p>Description</p>
+//       </div>
+//     );
+// }
+
+const cardEl = document.createElement('div');
+cardEl.classList.add('card');
+
+// arrow fun
+const CardComponent = () => (
+  <div class='card'>
+    <h1>Title</h1>
+    <p>Description</p>
+  </div>
+);
