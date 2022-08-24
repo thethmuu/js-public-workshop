ab1fd03a56688ad59ce41a3ba18e8c30b3cc205d
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 20:21:56 +0630

    update

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 'use strict';
 
 console.log('Hello JS');
+
+const users = [
+  {
+    id: 1,
+    name: 'Thet',
+  },
+  {
+    id: 2,
+    name: 'Hmuu',
+  },
+  {
+    id: 3,
+    name: 'John',
+  },
+];
+// find
+// return -> only one item
+const userWithIDone = users.findIndex((user) => user.id === 4);
+console.log(userWithIDone);
+// findIndex
+// return index
+// return -1 if not exist
+if (userWithIDone >= 0) {
+  console.log('it exists');
+} else {
+  console.log('not exist');
+}
