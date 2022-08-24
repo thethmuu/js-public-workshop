1666169a61a3c1c00ac2de85c20c6ae39eef0ada
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-08-24 19:04:13 +0630

    stsm & exp

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 'use strict';
 
 console.log('Hello JS');
+// statement
+// expression
+let newVar, newVarB;
+
+if (status === 'save') {
+    return <button>Save</button>;
+} else (status === 'update') {
+    return <button>Update</button>;
+}
+
+// statements cannot be wrriten in JSX
+status === 'save' ? <button>Save</button> : <button>Update</button>;
+
+function newButton() {
+    return (
+        status === 'save' ? <button>Save</button> : <button>Update</button>
+    )
+}
